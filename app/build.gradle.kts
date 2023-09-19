f911a39ba59b879d54933c5c761ed45d23359d9b
Tnasrul <nasrultoriq@gmail.com>
2023-09-20 00:42:05 +0700

    navigation animasi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,20 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs")
 }
 
 android {
     namespace = "com.dicoding.mynavgation"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dicoding.mynavgation"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
